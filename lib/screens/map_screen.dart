6edca0954af278494e7712521fde4b4aa75fb8b3
Mohammed Abdulkareem Hamid <hamidm@cip.ifi.lmu.de>
2025-08-31 23:12:40 +0200

    Load map using berlin as the startingpoint, add first draft of dark mode but WIP

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
-// TODO: get fluttermap and gps in here
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
 
@@ -11,16 +12,41 @@ class MapScreen extends StatefulWidget {
 class _MapScreenState extends State<MapScreen> {
   @override
   Widget build(BuildContext context) {
+
+    // Check  if dark mode is enabled
+    bool isDarkMode = Theme.of(context).brightness == Brightness.dark;
+    String tileUrl = isDarkMode
+        ? 'https://cartodb-basemaps-a.global.ssl.fastly.net/dark_all/{z}/{x}/{y}{r}.png'
+        : 'https://cartodb-basemaps-a.global.ssl.fastly.net/light_all/{z}/{x}/{y}{r}.png';
+
     return Scaffold(
-      appBar: AppBar(title: const Text("Wantr")),
-      body: const Center(
-        child: Text("Map goes here"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // TODO: Open dialog to add a new post
-        },
-        child: const Icon(Icons.add_location),
+      appBar: AppBar(title: const Text("Wantr Map")),
+      body: FlutterMap(
+        options: MapOptions(
+          initialCenter: LatLng(52.5200, 13.4050), // Use Berlin as startpoint
+          initialZoom: 13,
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: tileUrl,
+            subdomains: const ['a', 'b', 'c', 'd'],
+            userAgentPackageName: 'com.sonding.wantr',
+          ),
+          MarkerLayer(
+            markers: [
+              Marker(
+                point: LatLng(52.5200, 13.4050),
+                width: 80,
+                height: 80,
+                child: const Icon(
+                  Icons.location_on,
+                  color: Colors.red,
+                  size: 40,
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
